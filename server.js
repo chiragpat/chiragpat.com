624fccc38ef90572c23b2c402d70434105ca7bd9
Chirag Patel <chiragpat@gmail.com>
2013-09-12 01:32:43 -0500

    server file for subdomains

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    http    = require('http'),
+    Swarm = require('./Swarm/app.js');
+
+var app = express();
+
+
+app.use(express.vhost('swarm.*', Swarm.app));
+app.use(app.router);
+
+app.get('/', function (req, res) {
+  res.end('Hello World from root');
+});
+
+var server = http.createServer(app).listen(3000, function () {
+  console.log('Express server listening on port ' + 3000);
+});
+
+Swarm.socketServer(server);
